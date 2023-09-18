fa2e02330b89fb252ad0017d985d7ceb236c938a
Jeison Munoz <jmunozbetancourt@gmail.com>
2023-09-18 09:40:44 -0500

    Exercise1

---
+++ exercise1.js
@@ -0,0 +1,9 @@
+function Student(name,age,subjects) {
+    this.name = name
+    this.age = age
+    this.subjects = subjects
+}
+
+const student1 = new Student('jeison',27,['espanol','matematica','ingles'])
+
+console.log(student1)
